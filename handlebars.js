06fdd916fc5e3c50e9988486aeac4bcf844abe4a
nathaniel <nathaniel.daniel23@outlook.com>
2017-06-09 12:37:48 -0700

    add rendering engine

---
+++ handlebars.js
@@ -0,0 +1,11 @@
+const exhbs = require('express-handlebars');
+const config = require('./config.js');
+const handlebars = exhbs.create(config.exhbs);
+module.exports = handlebars;
+module.exports.attach = function(array){
+  for(var i in array){
+    array[i].engine('.hbs', handlebars.engine);
+    array[i].set('view engine', '.hbs');
+    array[i].enable('view cache');
+  }
+}
--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const handlebars = require('./handlebars.js');
+const config = require('./config.js');
 const app = express();
+const PORT = config.PORT;
+
+handlebars.attach([app]);
 
 app.use(function(req, res, next){
-  console.log(req.path);
+  console.log('Path: ' + req.path);
   next();
 });
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.render('home');
 });
 
-app.listen('8080');
+app.listen(PORT, function(){
+    console.log('Server running on ' + PORT);
+});
